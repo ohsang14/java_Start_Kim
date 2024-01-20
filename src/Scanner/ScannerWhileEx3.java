825305110bc11b847548342a70146832eda7db58
ohsang14 <ast6108@naver.com>
2024-01-20 17:23:19 +0900

    study

--- src/Scanner/ScannerWhileEx3.java
+++ src/Scanner/ScannerWhileEx3.java
@@ -4,23 +4,44 @@ import java.util.Scanner;
 
 public class ScannerWhileEx3 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
         int sum = 0;
         int count = 0;
         int input = 0;
+
+        System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요");
         while(true){
-            System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요");
-            input = scan.nextInt();
-            if (input == -1) {
+            input = scanner.nextInt();
+            if(input == -1){
                 break;
             }
             sum += input;
-            count ++;
+            count++;
         }
-        double avarge = (double) sum / count;
-        System.out.println("합계" + sum);
-        System.out.println("평균" + avarge);
-
+        double avarage = (double)sum/count;
+        System.out.println("입력한 숫자들의 합계:" + sum);
+        System.out.println("입력한 숫자들의 평균:" + avarage);
     }
 }
+
+
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//
+//        int sum = 0;
+//        int count = 0;
+//        int input = 0;
+//        while(true){
+//            System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요");
+//            input = scan.nextInt();
+//            if (input == -1) {
+//                break;
+//            }
+//            sum += input;
+//            count ++;
+//        }
+//        double avarge = (double) sum / count;
+//        System.out.println("합계" + sum);
+//        System.out.println("평균" + avarge);
+
+
--- src/Scanner/ScannerWhileEx4.java
+++ src/Scanner/ScannerWhileEx4.java
@@ -4,35 +4,74 @@ import java.util.Scanner;
 
 public class ScannerWhileEx4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int totalCost = 0;
 
-        while (true){
+        while(true){
             System.out.println("1: 상품 입력, 2: 결제, 3: 프로그램 종료");
-            int option = sc.nextInt();
+            int option = input.nextInt();
 
-            if (option == 1 ){
-                sc.nextLine();
+            if (option == 1){
+                input.nextLine();
 
                 System.out.println("상품명을 입력하세요: ");
-                String name = sc.nextLine();
-                System.out.println("상품 가격을 입력하세요: ");
-                int price = sc.nextInt();
-                System.out.println("구매 수량을 입력하세요: ");
-                int quantity = sc.nextInt();
+                String product = input.nextLine();
+
+                System.out.println("상품의 가격을 입력하세요: ");
+                int price = input.nextInt();
+
+                System.out.println("구매 수량을 입력하세요:");
+                int quantity = input.nextInt();
 
                 totalCost += price * quantity;
-                System.out.println("상품명: "+ name + " 가격:" + price+ "수량: "+
-                        quantity+ "합계: "+ price * quantity);
+
+                System.out.println("상품명: "+ product + " 가격: " + price+ " 수량: "+
+                        quantity+ " 합계: "+ price * quantity);
             }else if(option == 2){
-                System.out.println("총비용: " + totalCost);
+                System.out.println("총 비용: "+ totalCost);
                 totalCost = 0;
-            } else if (option == 3) {
+            }else if(option == 3){
                 System.out.println("프로그램을 종료합니다.");
                 break;
             }else{
-                System.out.println("올바른 옵션을 선택해주세요!");
+                System.out.println("올바른 옵션을 선택해주세요.");
             }
         }
+
+
+
+
     }
 }
+//        Scanner sc = new Scanner(System.in);
+//        int totalCost = 0;
+//
+//        while (true){
+//            System.out.println("1: 상품 입력, 2: 결제, 3: 프로그램 종료");
+//            int option = sc.nextInt();
+//
+//            if (option == 1 ){
+//                sc.nextLine();
+//
+//                System.out.println("상품명을 입력하세요: ");
+//                String name = sc.nextLine();
+//                System.out.println("상품 가격을 입력하세요: ");
+//                int price = sc.nextInt();
+//                System.out.println("구매 수량을 입력하세요: ");
+//                int quantity = sc.nextInt();
+//
+//                totalCost += price * quantity;
+//                System.out.println("상품명: "+ name + " 가격:" + price+ "수량: "+
+//                        quantity+ "합계: "+ price * quantity);
+//            }else if(option == 2){
+//                System.out.println("총비용: " + totalCost);
+//                totalCost = 0;
+//            } else if (option == 3) {
+//                System.out.println("프로그램을 종료합니다.");
+//                break;
+//            }else{
+//                System.out.println("올바른 옵션을 선택해주세요!");
+//            }
+//        }
+//    }
+//}
